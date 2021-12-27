3586cc8830338d3d54918b36662221d178db4884
Gustav <gustav.elmqvist@gmail.com>
2021-12-27 20:28:01 +0100

    2015 day6

---
+++ 2015/06.py
@@ -0,0 +1,54 @@
+# Solution took 00:23:11
+
+def part1(indata):
+    grid = [[0] * 1000 for x in range(1000)]
+    for row in indata:
+        data = row.split()
+        if data[0] == 'toggle':
+            state = 'toggle'
+        else:
+            state = data[1]
+        x0, y0 = [int(e) for e in data[-3].split(',')]
+        x1, y1 = [int(e) for e in data[-1].split(',')]
+        for x in range(x0, x1+1):
+            for y in range(y0, y1+1):
+                if state == 'on' or state == 'toggle' and grid[y][x] == 0:
+                    grid[y][x] = 1
+                elif state == 'off' or state == 'toggle' and grid[y][x] == 1:
+                    grid[y][x] = 0
+    lit = 0
+    for row in grid:
+        lit += sum(row)
+    return lit
+
+
+def part2(indata):
+    grid = [[0] * 1000 for x in range(1000)]
+    for row in indata:
+        data = row.split()
+        if data[0] == 'toggle':
+            state = 'toggle'
+        else:
+            state = data[1]
+        x0, y0 = [int(e) for e in data[-3].split(',')]
+        x1, y1 = [int(e) for e in data[-1].split(',')]
+        for x in range(x0, x1+1):
+            for y in range(y0, y1+1):
+                if state == 'on':
+                    grid[y][x] += 1
+                elif state == 'off' and grid[y][x] > 0:
+                    grid[y][x] -= 1
+                elif state == 'toggle':
+                    grid[y][x] += 2
+    lit = 0
+    for row in grid:
+        lit += sum(row)
+    return lit
+
+
+if __name__ == "__main__":
+    with open("input/06", "r") as f:
+        indata = f.read().split('\n')[:-1]
+
+    print("part1:", part1(indata))
+    print("part2:", part2(indata))
