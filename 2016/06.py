eeceedf3fbd20b55ab6ad3563c61fa70f0390a36
Gustav <gusel725@student.liu.se>
2023-10-20 18:18:30 +0200

    2016 day 6

---
+++ 2016/06.py
@@ -0,0 +1,44 @@
+from aocd.models import Puzzle
+from aocd import submit
+import time
+
+directory, filename = __file__.split('/')[-2:]
+YEAR, DAY = int(directory), int(filename[:-3])
+
+puzzle = Puzzle(day=DAY, year=YEAR)
+indata = puzzle.input_data.split('\n')
+
+def part1():
+    result = ''
+    # Transpose the indata to get a list of columns instead
+    columns = [''.join(row[i] for row in indata) for i in range(len(indata[0]))]
+    for column in columns:
+        # Count all the chars in the column
+        chars = {char: column.count(char) for char in set(column)}
+        result += max(chars, key=chars.get)
+    return result
+
+
+def part2():
+    result = ''
+    # Transpose the indata to get a list of columns instead
+    columns = [''.join(row[i] for row in indata) for i in range(len(indata[0]))]
+    for column in columns:
+        # Count all the chars in the column
+        chars = {char: column.count(char) for char in set(column)}
+        result += min(chars, key=chars.get)
+    return result
+
+
+if __name__ == "__main__":
+    t0 = time.time()
+
+    part1_answer = part1()
+    print("\npart1:", part1_answer)
+    submit(part1_answer, part="a", day=DAY, year=YEAR)
+
+    part2_answer = part2()
+    print("\npart2:", part2_answer)
+    submit(part2_answer, part="b", day=DAY, year=YEAR)
+
+    print("\ntime:", time.time()-t0)
