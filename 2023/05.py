c5b92030a7a3bc2314f3e2c3407806136ffc3490
Gustav Elmqvist <gustav@seapattern.com>
2023-12-05 14:11:06 +0100

    2023 day 5 - If You Give A Seed A Fertilizer [clean]

--- 2023/05.py
+++ 2023/05.py
@@ -6,142 +6,75 @@ import re
 import time
 
 
-def parse_input(indata):
-    initial_seeds = [('seed', int(x)) for x in re.findall(r"\d+", indata[0])]
-    maps = {}
-    for mapp in indata[1:]:
-        mapp = mapp.split("\n")
-        source, dest = mapp[0].split(' ')[0].split('-to-')
-        x = [dest]
-        for row in mapp[1:]:
-            dest_range, source_range, range_length = [int(x) for x in re.findall(r"\d+", row)]
-            x.append((dest_range, source_range, range_length))
-        maps[source] = x
-    return initial_seeds, maps
+def parse_input(indata, part="a"):
+    seeds = [int(x) for x in re.findall(r"\d+", indata[0])]
+    if part == "b":
+        seeds = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
 
+    maps = []
+    for section in indata[1:]:
+        maps.append([[*map(int, row.split())] for row in section.split("\n")[1:]])
 
-def get_next_seed(category, number, maps):
-    dest = maps[category][0]
-    res = []
-    for i0, i1, length in maps[category][1:]:
-        if i1 <= number < i1+length:
-            res.append((dest, i0 + (number - i1)))
-    return res or [(dest, number)]
+    return seeds, maps
 
 
 def overlaps(a0, a1, b0, b1):
-    overlap = None
-    non_overlap = []
-    # a: [   ]
-    # b:   [ ...
-    if a0 <= b0 < a1:
-        overlap = (b0, min(a1, b1))
+    overlap = (max(a0, b0), min(a1, b1)) if a0 <= b0 < a1 or b0 <= a0 < b1 else None
+    non_overlap = (a0, a1) if a1 <= b0 or b1 <= a0 else (b1, a1) if b1 < a1 else None
 
-    # a:   [ ...
-    # b: [   ]
-    if b0 <= a0 < b1:
-        overlap = (a0, min(a1, b1))
-
-    # a: [   ]
-    # b:       [   ]
-    if a1 <= b0 or b1 <= a0:
-        non_overlap += [(a0, a1)]
-
-    # a: [   ]
-    # b:  [   ]
-    if a0 < b0 < a1 < b1:
-        non_overlap += [(a0, b0)]
-
-    # a:  [   ]
-    # b: [   ]
-    elif b0 < a0 < b1 < a1:
-        non_overlap += [(b1, a1)]
-
-    # a: [     ]
-    # b:  [   ]
-    elif a0 < b0 < b1 < a1:
-        non_overlap += [(a0, b0), (b1, a1)]
-    
     return overlap, non_overlap
 
 
+def get_next_range(seed, conversion_map):
+    if not conversion_map:
+        return [seed]
 
-def get_next_range(category, r0, r1, maps):
-    dest = maps[category][0]
-    res = []
-    ranges = [(r0, r1)]
-    for d, s, l in maps[category][1:]:
-        new_ranges = []
-        for r0, r1 in ranges:
-            overlap, non_overlap = overlaps(r0, r1, s, s+l)
-            if overlap:
-                a, b = overlap
-                res += [(dest, a-s+d, b-s+d)]
-            new_ranges += non_overlap
-        ranges = new_ranges
+    dest, source, length = conversion_map[0]
 
-    # leftover ranges
-    for non_overlap in ranges:
-        a, b = non_overlap
-        res += [(dest, a, b)]
+    overlap, non_overlap = overlaps(seed[0], seed[1], source, source + length)
 
-    return res
+    result = []
+    if overlap:
+        result += [(overlap[0] - source + dest, overlap[1] - source + dest)]
+    if non_overlap:
+        result += get_next_range(non_overlap, conversion_map[1:])
+
+    return result
 
 
-def part1(indata):
-    seeds, maps = parse_input(indata)
+def traverse(seed, maps, part):
+    if not maps:
+        return seed if part == "a" else seed[0]
 
-    locations = []
-    while seeds:
-        category, number = seeds.pop()
-        if category == 'location':
-            locations.append(number)
-            continue
-        seeds += get_next_seed(category, number, maps)
-    return min(locations)
+    if part == "a":
+        for dest, source, length in maps[0]:
+            if source <= seed < source + length:
+                return traverse(seed - source + dest, maps[1:], part)
+        return traverse(seed, maps[1:], part)
+
+    return min(traverse(s, maps[1:], part) for s in get_next_range(seed, maps[0]))
 
 
-def part2(indata):
-    seed_ranges, maps = parse_input(indata)
-
-    seeds = []
-    i = 0
-    while i < len(seed_ranges):
-        start = seed_ranges[i][1]
-        length = seed_ranges[i+1][1]
-        seeds.append(('seed', start, start+length))
-        i += 2
-
-    locations = []
-    while seeds:
-        category, a, b = seeds.pop()
-        if category == 'location':
-            locations.append(a)
-            continue
-        seeds += get_next_range(category, a, b, maps)
-    return min(locations)
+def solve(indata, part):
+    seeds, maps = parse_input(indata, part=part)
+    return min(traverse(seed, maps, part) for seed in seeds)
 
 
 if __name__ == "__main__":
     t0 = time.time()
 
-    directory, filename = __file__.split('/')[-2:]
+    directory, filename = __file__.split("/")[-2:]
     YEAR, DAY = int(directory), int(filename[:-3])
 
     puzzle = Puzzle(day=DAY, year=YEAR)
     puzzle_input = puzzle.input_data.split("\n\n")
-    example_input = puzzle.examples[0].input_data.split("\n\n")
 
-    ex1 = part1(example_input)
-    ex1_answer = int(puzzle.examples[0].answer_a)
-    assert ex1 == ex1_answer, f"Ex1: expected {ex1_answer}, got {ex1}"
-
-    part1_answer = part1(puzzle_input)
+    part1_answer = solve(puzzle_input, part="a")
     print("\npart1:", part1_answer)
     submit(part1_answer, part="a", day=DAY, year=YEAR)
 
-    part2_answer = part2(puzzle_input)
+    part2_answer = solve(puzzle_input, part="b")
     print("\npart2:", part2_answer)
     submit(part2_answer, part="b", day=DAY, year=YEAR)
-        
-    print("\ntime:", time.time()-t0)
+
+    print("\ntime:", time.time() - t0)
