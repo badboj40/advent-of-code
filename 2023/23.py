8bc44cbb3b086e5f9133a87196d594e535679595
Gustav Elmqvist <gustav@seapattern.com>
2023-12-23 09:49:33 +0100

    2023 day 23 - the longest hike [iterative dfs]

--- 2023/23.py
+++ 2023/23.py
@@ -1,13 +1,8 @@
 # 23   00:23:46    849      0   01:22:45    851      0
 
-# Slow again, runs in ~22 seconds with pypy3
+# Slow again, runs in ~13 seconds with pypy3
 
 from aocd.models import Puzzle
-import time
-
-import sys
-
-sys.setrecursionlimit(10000)
 
 
 def get_neighbors(grid, y, x, part):
@@ -42,15 +37,22 @@ def contract(graph):
             graph[pos2][pos1] = dist
 
 
-def dfs(graph, pos, goal, visited, length):
-    if pos == goal:
-        return length
-    paths = [0]
-    for neighbor in graph[pos]:
-        if neighbor not in visited:
-            dist = graph[pos][neighbor]
-            paths += [dfs(graph, neighbor, goal, visited | {pos}, length + dist)]
-    return max(paths)
+def dfs(graph, start, goal):
+    stack = [(start, {start}, 0)]
+    longest_path = 0
+
+    while stack:
+        pos, path, path_length = stack.pop()
+
+        if pos == goal:
+            longest_path = max(longest_path, path_length)
+            continue
+
+        for neighbor, dist in graph[pos].items():
+            if neighbor not in path:
+                stack.append((neighbor, path | {neighbor}, path_length + dist))
+
+    return longest_path
 
 
 def solve(indata, part):
@@ -58,11 +60,9 @@ def solve(indata, part):
     if part == "b":
         contract(graph)
     goal = (len(indata) - 1, indata[-1].index("."))
-    return dfs(graph, (0, 1), goal, set(), 0)
+    return dfs(graph, (0, 1), goal)
 
 
 if __name__ == "__main__":
-    t0 = time.time()
     input_data = Puzzle(day=23, year=2023).input_data.split("\n")
     print(f"\npart1: {solve(input_data, 'a')}\npart2: {solve(input_data, 'b')}")
-    print("\ntime:", time.time() - t0)
